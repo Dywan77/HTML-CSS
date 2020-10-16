1d4795629ffd9fa04970c2f0fca3d970c9072a93
Dywan <59533458+Dywan77@users.noreply.github.com>
2020-10-16 23:08:38 +0200

    Add files via upload

---
+++ Ascii-Camera/me.js
@@ -0,0 +1,39 @@
+/*fffffftttttt111iiiii;;;::,,..........  ....................,,,,;f;     ..;,t;;i1t@L@f   G8888@@@@G.f
+,,,,.................8@@@@888800GCfti:;t. ..ii1111111tttttttt..i1:      :t:.;i. 80t0f   8,8@@@C:GGL@
+8@8000GGCCCLLLCGG,...0Lft1i;:,.             ..,:ii;1fC0@@@@@@f:;;,      .,:.L 1:8@@81   @@@CG@8@8.  
+0CCCLLCftLCLffLt1,.. @0GCLLGL1tGi.tL ;L:    it0f1fCG8@@@@@@@@@@@@i.    . ;  @ i 88C@1   @@@@@.      
+GG00G0GCLCGCLLCf1... iCCfCGGGCCCGGLGL  ,   .0@8CG08@@@@@@@@@@@@@@@C.   .;,, @.t @8G@;   Gt          
+LG0GCGCCfLCLLLCff,...0CCCCCGGLCCCCGGCC1C  .i1C8@@@@@@@@@@@@@@@@@@@@@8LffLLLLffffffft1               
+LC0GGCCCLLCLLLCLf:...0LCCCCGGCCGCCG0CGCC;00@880G08@@@@@@@@@@@@@@@@@@@@CLLfLCLLLftfft0f              
+CG00GGCCLLCLLLGff:...0CCCCCGGCCGCC00C0G8@@@@@0LLCCCCCGGGC0@@@@@@@@@@@@@fttfLLftt1tt10L              
+GG0000GCLLCCLLGLf:.,.0CGCGGGCCCGCG08G00@@@@@8GLCLffttt1111tLG8@@@@@@@@@fttfLLfft1ttt8L              
+@@@8000CLCGGLCGCL;.,.GCGGG00GGGGGG08G00@@@@0CCLLfttiii;;;;i111tLC8@@@@@CftfLLftttttt8L              
+0@@@880GCCGGCC0CLi.:.C0GGG80GGGGGG08G08@@@0CCLLft11ii;::::;;ii11fC8@@@@LftfLLffttftt@L              
+@@@@@@8GGGG0GG8CL1::,L00GG00GGGGG008G08@@@888@@8L1i;::,,::;iii1tfLC8@@@LffLCLffftfff@L              
+@@@@@@@0GG80G0Gffii:,f88808800000080GLt@@0GGCLfL0@@Ci,,,:,:;;i1tfLC@@@CLffLCLLfftfff@L              
+@@@@@8@0G88@88LfG1::::::f@@8000008800LG@8CCCCLt1iiitC8LtfG08@8800CG@@GLLLfLCCLLffLff@L              
+@@@@@@@@@@8@@0CCGt;;;,,:f08@8000008@@L0GLLCC;;;;;;;;G8G@8t1111f0@@8@0CCLLLCCCLLffLLL@L              
+@GG888@@@@G@@@@@8Gtt;,,;@@@@@@8C8@@@@0@ftGLftt1ttfttC1t0;;;;;CCGC8@@@0CCLLCCCLLLLCLL@f              
+@CG08@@@@@L@@@@@@@@C,,,C@@@@@0::;@@@@fftt1iifffLC00Ct1t0i;;;f00CC0G@GCCCCCCGGCCLLCLC@f              
+CLCGCCCGGLtfLLLLCCL;,,;ffffft:,,;t111tftti;;;;;;iCGLtttCGCfttffLLG@0GGGCCCGGCCCLCCCC@t              
+;;;::::::::::::::::,.,i:::::,,,;,..,,,ifti;::::;tfi;,:iftiitG88@80000GGGGCGGGGCCCCCG@t... .         
+..,....,,.,,,,,,,,...,:,..:,,,:........tf1;;;;;iifL1i1t1;;;iiitC888800GGGGG0GGCCCGGG@1.......       
+,,,,,,,,,,,,,,,::::::i,,,,..,i.........1ff1111tfLfi;i11i;;;i1fGG0088000GGGG0GGGGGGGG@1.........     
+fi:::::::,:;;;1iLLft1t:;,.,:;,,,:t080GCC0ft1iifCCft111ttt11tC8CCCG0000000G0800GGG0G0@1........ . .  
+0GG01i;:,.ii::18G1fLt1i:,,;:,,f8@@0CCCC08Cft1i;;;;;;1tt11fL8@8@80088@@@00008000G0000@1............. 
+8LfffL11;.:t::;:.;fft1;::i;:,1CCCCCCGCG@8CCL1i;;:::;i1tfCG0@@8@80888@8@@G08888000000@i............. 
+GfLLf1i;i:;i::i;i;tCGt1;tG0GCCCCCCCC000@8CLCCti;,::;1fLGCC0@@@88888880G00008@@800808@i.,............
+C0GGGt:,,i;t::;,,,ifL1i1CCC08880GG00088@@GCCCLfftttLCCCLfL8@@@88888888088880CCCC8888@i,,,,,.........
+@@@@@i,,,,,,;;ii,,:10CLGGGCCG8@@0CG0008@@80CGGL1i1fLLCLfLG@@@8888888@@8@@8880CCCGG00@;,,,,,,,,,,,...
+@@@@@;,..,,1t:,:,,,;t08GG000008@@8008888@@@0GGCGCGCCCLLLG@@@8888888@@@@@@@@@800000800;,,:,,,,,,,,,..
+@@@@@;,.,,;i:,,,,,,;0@@88088888@@@@80888@@@@8G00GGGCCC8@@@@888888@@8@@@@8@@@8888888880G;::,,,:,:,,,,
+@@@@0;,,,,::,,,,,:iG88@@@8888888@@8880088@@8@@@8@8888@@@@800888@@@@@@@888@@@@88888888880Gi::::,:,,,,
+@@88ft;,,,,::;:;itG808@@@@8888@@@@88888008@@8888888888@@8888888@@@@@@@888@@@@8@8@@88888800C:::::::::
+8@@@G1;:::,,:;i1088888@@@@@8@@@@@@8888@8808@@@@888888@@888888@@@@@@@@@8888@@@@8@@@8@@@888800i;::::::
+088@@8f1;:::itL@88@@8@@@@@@8@@@8888888@@8@88@@@@@@@@@@888@@@@@@@@@@8@@@8@@@@@@@@@@@@@@@888880Gi;;:::
+8888888@@@@88@@@08@@8@@@@@@@88@888888@@@@@888@@@@@@@@@@@@@@@@@@8@@@@@@@@@@@@@@@@@@@@@@@@8@88880Ci;;;
+8888888@88888@8@@@@88@@@@@@8@888888888@@@88@@@8@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@88880GCi
+88888@88888888@@@@@88@@@@@@@@@@888@8@@@@@@888888@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@888880G
+888888@888888@@@@@@@@@@@@@@@@@@@@@@88@@@@8888888@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@8888
+888@@@8@@888@@@@@@@@@@@@@@@@@@@@@@8@@@@@@@@@8@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+*/
---
+++ Ascii-Camera/script/camera.js
@@ -0,0 +1,122 @@
+
+
+var camera = (function() {
+	var options;
+	var video, canvas, context;
+	var renderTimer;
+
+	function initVideoStream() {
+		
+			video = document.createElement("video");
+			video.setAttribute('width', options.width);
+			video.setAttribute('height', options.height);
+			video.setAttribute('playsinline', 'true');
+			video.setAttribute('webkit-playsinline', 'true');
+
+		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+		window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+
+
+
+		if (navigator.getUserMedia) {
+			navigator.getUserMedia({
+				video: true,
+				audio: false,
+			}, function(stream) {
+				options.onSuccess();
+
+				if (video.mozSrcObject !== undefined) { 
+					video.mozSrcObject = stream;
+				} else {
+					video.srcObject = stream;
+				}
+
+				initCanvas();
+			}, options.onError);
+		} else {
+			options.onNotSupported();
+		}
+	}
+
+
+
+
+	function initCanvas() {
+		canvas = options.targetCanvas || document.createElement("canvas");
+		canvas.setAttribute('width', options.width);
+		canvas.setAttribute('height', options.height);
+
+		context = canvas.getContext('2d');
+
+		if (options.mirror) {
+			context.translate(canvas.width, 0);
+			context.scale(-1, 1);
+		
+		}
+	}
+
+
+
+
+	function startCapture() {
+		video.play();
+
+		renderTimer = setInterval(function() {
+			try {
+				context.drawImage(video, 0, 0, video.width, video.height);
+				options.onFrame(canvas);
+			} catch (e) {
+			
+			}
+		}, Math.round(1000 / options.fps));
+	}
+
+	function stopCapture() {
+		pauseCapture();
+		if (video.mozSrcObject !== undefined) {
+			video.mozSrcObject = null;
+		} else {
+
+			video.srcObject = null;
+		}
+	}
+
+
+	function pauseCapture() {
+		if (renderTimer) clearInterval(renderTimer);
+		video.pause();
+
+
+
+	}
+
+	return {
+		init: function(captureOptions) {
+			var doNothing = function(){};
+
+
+
+			options = captureOptions || {};
+			options.fps = options.fps || 30;
+			options.width = options.width || 640;
+			options.height = options.height || 480;
+			options.mirror = options.mirror || false;
+			options.targetCanvas = options.targetCanvas || null; 
+
+			options.onSuccess = options.onSuccess || doNothing;
+			options.onError = options.onError || doNothing;
+			options.onNotSupported = options.onNotSupported || doNothing;
+			options.onFrame = options.onFrame || doNothing;
+
+			initVideoStream();
+		},
+
+
+
+
+		start: startCapture,
+		pause: pauseCapture,
+		stop: stopCapture
+	};
+})
+();
